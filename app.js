15c66a8b77586122363be31da0be9129827425a3
Rick712 <rick.buter@gmail.com>
2019-01-09 14:40:52 +0100

    shit is working oy

--- app.js
+++ app.js
@@ -3,32 +3,56 @@ const app = express()
 const ejs = require('ejs')
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const data = require('./data.json')
+const books = require('./data.json')
 const genres = require('./genres.json')
 
-genres.forEach(el => {
-    console.log(el.title)
-});
-
 const port = 3000
+
+let likedGenres = [], 
+    relatedBooks = []
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-    res.render('index', {data: data, genres: genres})
+    res.render('index', {data: books, genres: genres})
+    io.on('send', function() {
+        res.render('result', {books: relatedBooks})
+        checkBook()
+    })
 })
 
 io.on('connection', function(socket) {
-    const likedGenres = []
     socket.on('likeGenre', function(genre) {
         likedGenres.push(genre)
+    })
+    socket.on('send', function () {
         console.log(likedGenres)
     })
 })
 
+const checkBook = function() {
+
+    likedGenres.forEach((genre) => {
+        books.forEach((book) => {
+            if(book.Genre.includes(genre)) {
+                relatedBooks.push(book)
+            }
+        })
+    })
+    console.log(relatedBooks)
+    return relatedBooks
+}
+
 app.get('/admin', function (req, res) {
     res.render('admin', {genres: genres})
+    likedGenres = []
+    relatedBooks = []
+})
+
+app.get('/result', function (req, res) {
+    checkBook()
+    res.render('result', {books: relatedBooks})
 })
 
 http.listen(port, function () {
